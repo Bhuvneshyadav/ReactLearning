a78e9d3d734d450a294651cd4ed213ae03022200
Bhuvnesh <bhuvnesh2019@gmail.com>
2025-06-02 03:46:39 +0530

    change in dev branch

--- Investment Calculator/src/component/header.jsx
+++ Investment Calculator/src/component/header.jsx
@@ -2,6 +2,7 @@ export default function Header() {
 
     return (
         <div>
+            <h1>Bhuvnesh</h1>
             <header id="header">
                 <h1>React Investment Calculator</h1>
                 <img src="investment-calculator-logo.png" alt="investment calculator"></img>
