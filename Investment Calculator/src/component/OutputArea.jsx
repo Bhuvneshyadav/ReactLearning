3a7cb560a50ac9d1f5d37adca8e830369793bc5a
Bhuvnesh <bhuvnesh2019@gmail.com>
2025-06-02 02:13:59 +0530

    new Change

---
+++ Investment Calculator/src/component/OutputArea.jsx
@@ -0,0 +1,47 @@
+import { calculateInvestmentResults, formatter } from "../util/investment";
+
+export default function OutputArea({ input }) {
+    const inputInvst = {
+        initialInvestment: input.initial,
+        annualInvestment: input.annual,
+        expectedReturn: input.expected,
+        duration: input.duration
+    };
+
+
+    const resultsData = calculateInvestmentResults(inputInvst);
+    const initialInvestment =
+        resultsData[0].valueEndOfYear -
+        resultsData[0].interest -
+        resultsData[0].annualInvestment;
+
+
+
+    return (
+        <table id="result">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest (year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {resultsData.map(yearData => {
+                    const totoalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
+                    const totalAmountInvested = yearData.valueEndOfYear - totoalInterest;
+
+                    return <tr key={yearData.year}>
+                        <td>{yearData.year}</td>
+                        <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                        <td>{formatter.format(yearData.interest)}</td>
+                        <td>{formatter.format(totoalInterest)}</td>
+                        <td>{formatter.format(totalAmountInvested)}</td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
+    );
+}
---
+++ Investment Calculator/src/component/header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+
+    return (
+        <div>
+            <header id="header">
+                <h1>React Investment Calculator</h1>
+                <img src="investment-calculator-logo.png" alt="investment calculator"></img>
+            </header>
+
+        </div>
+    );
+}
